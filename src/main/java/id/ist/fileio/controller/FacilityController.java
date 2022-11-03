8a59a348e2cab25463621ea87f86507b02094f7a
hazuhdi <habibzuhdi3@gmail.com>
2022-11-03 14:51:54 +0700

    fixing bug + implements beanutils for update facility

--- src/main/java/id/ist/fileio/controller/FacilityController.java
+++ src/main/java/id/ist/fileio/controller/FacilityController.java
@@ -9,12 +9,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 
 import id.ist.fileio.model.Facility;
 import id.ist.fileio.service.FacilityService;
@@ -26,7 +33,10 @@ import lombok.extern.slf4j.Slf4j;
 public class FacilityController {
 
 	@Autowired
-	FacilityService facilityService;
+	private FacilityService facilityService;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	@GetMapping
 	public ResponseEntity<List<Facility>> getAllFacility() {
@@ -68,4 +78,18 @@ public class FacilityController {
 //		HttpStatus responseHTTPStatus = (facilCheck != null) ? HttpStatus.OK: HttpStatus.NO_CONTENT;
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
+	
+	@PatchMapping(path = "/patch/{id}", consumes = "application/json-patch+json")
+	public Facility updateCustomer(@PathVariable Long id, @RequestBody JsonPatch patch) {
+	    try {
+	        Facility facility = facilityService.findById(id);
+	        JsonNode patched = patch.apply(objectMapper.convertValue(facility, JsonNode.class));
+	        Facility patchedFacility = objectMapper.treeToValue(patched, Facility.class);
+	        facilityService.editFile(id, patchedFacility);
+	        return patchedFacility;
+	    } catch (JsonPatchException | JsonProcessingException e) {
+	        throw new InvalidDataException();
+	    }
+	}
+
 }
---
+++ src/main/java/id/ist/fileio/controller/InvalidDataException.java
@@ -0,0 +1,10 @@
+package id.ist.fileio.controller;
+
+public class InvalidDataException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7835982479116538901L;
+
+}
